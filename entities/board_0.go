7d012e34d1aed92aebd862da624f8b2bdc744d20
むずでょ <muzudho1@gmail.com>
2022-01-22 05:03:27 +0900

    board_0

---
+++ entities/board_0.go
@@ -0,0 +1,180 @@
+package entities
+
+import (
+	"math/rand"
+)
+
+// Board0 - 盤。
+type Board0 struct {
+	data             []int
+	boardSize        int
+	sentinelWidth    int
+	sentinelBoardMax int
+	komi             float64
+	maxMoves         int
+	uctChildrenSize  int
+}
+
+// BoardSize - 何路盤か
+func (board Board0) BoardSize() int {
+	return board.boardSize
+}
+
+// SentinelWidth - 枠付きの盤の一辺の交点数
+func (board Board0) SentinelWidth() int {
+	return board.sentinelWidth
+}
+
+// SentinelBoardMax - 枠付きの盤の交点数
+func (board Board0) SentinelBoardMax() int {
+	return board.sentinelBoardMax
+}
+
+// Komi - コミ
+func (board Board0) Komi() float64 {
+	return board.komi
+}
+
+// MaxMoves - 最大手数
+func (board Board0) MaxMoves() int {
+	return board.maxMoves
+}
+
+// UctChildrenSize - UCTの最大手数
+func (board Board0) UctChildrenSize() int {
+	return board.uctChildrenSize
+}
+
+// ColorAt - 指定した交点の石の色
+func (board Board0) ColorAt(z int) int {
+	return board.data[z]
+}
+
+// ColorAtXy - 指定した交点の石の色
+func (board Board0) ColorAtXy(x int, y int) int {
+	return board.data[(y+1)*board.sentinelWidth+x+1]
+}
+
+// Exists - 指定の交点に石があるか？
+func (board Board0) Exists(tIdx int) bool {
+	return board.data[tIdx] != 0
+}
+
+// SetColor - 盤データ。
+func (board *Board0) SetColor(tIdx int, color int) {
+	board.data[tIdx] = color
+}
+
+// CopyData - 盤データのコピー。
+func (board Board0) CopyData() []int {
+	boardMax := board.SentinelBoardMax()
+
+	var boardCopy2 = make([]int, boardMax)
+	copy(boardCopy2[:], board.data[:])
+	return boardCopy2
+}
+
+// ImportData - 盤データのコピー。
+func (board *Board0) ImportData(boardCopy2 []int) {
+	copy(board.data[:], boardCopy2[:])
+}
+
+// GetZ4 - tIdx（配列のインデックス）を XXYY形式（3～4桁の数）の座標へ変換します。
+func (board Board0) GetZ4(tIdx int) int {
+	if tIdx == 0 {
+		return 0
+	}
+	y := tIdx / board.SentinelWidth()
+	x := tIdx - y*board.SentinelWidth()
+	return x*100 + y
+}
+
+// GetTIdxFromXy - x,y を tIdx（配列のインデックス）へ変換します。
+func (board Board0) GetTIdxFromXy(x int, y int) int {
+	return (y+1)*board.SentinelWidth() + x + 1
+}
+
+// GetEmptyTIdx - 空点の tIdx（配列のインデックス）を返します。
+func (board Board0) GetEmptyTIdx() int {
+	var x, y, tIdx int
+	for {
+		// ランダムに交点を選んで、空点を見つけるまで繰り返します。
+		x = rand.Intn(9)
+		y = rand.Intn(9)
+		tIdx = board.GetTIdxFromXy(x, y)
+		if !board.Exists(tIdx) {
+			break
+		}
+	}
+	return tIdx
+}
+
+func (board Board0) countLibertySub(tIdx int, color int, pLiberty *int, pStone *int) {
+	checkBoard[tIdx] = 1
+	*pStone++
+	for i := 0; i < 4; i++ {
+		z := tIdx + Dir4[i]
+		if checkBoard[z] != 0 {
+			continue
+		}
+		if !board.Exists(z) {
+			checkBoard[z] = 1
+			*pLiberty++
+		}
+		if board.data[z] == color {
+			board.countLibertySub(z, color, pLiberty, pStone)
+		}
+	}
+
+}
+
+// CountLiberty - 呼吸点を数えます。
+func (board Board0) CountLiberty(tIdx int, pLiberty *int, pStone *int) {
+	*pLiberty = 0
+	*pStone = 0
+	boardMax := board.SentinelBoardMax()
+	// 初期化
+	for tIdx2 := 0; tIdx2 < boardMax; tIdx2++ {
+		checkBoard[tIdx2] = 0
+	}
+	board.countLibertySub(tIdx, board.data[tIdx], pLiberty, pStone)
+}
+
+// TakeStone - 石を打ち上げ（取り上げ、取り除き）ます。
+func (board *Board0) TakeStone(tIdx int, color int) {
+	board.data[tIdx] = 0
+	for dir := 0; dir < 4; dir++ {
+		tIdx2 := tIdx + Dir4[dir]
+		if board.data[tIdx2] == color {
+			board.TakeStone(tIdx2, color)
+		}
+	}
+}
+
+// InitBoard - 盤の初期化。
+func (board *Board0) InitBoard() {
+	boardMax := board.SentinelBoardMax()
+	boardSize := board.BoardSize()
+	// G.Chat.Trace("# (^q^) boardMax=%d boardSize=%d\n", boardMax, boardSize)
+
+	// 枠線
+	for tIdx := 0; tIdx < boardMax; tIdx++ {
+		board.SetColor(tIdx, 3)
+	}
+
+	// G.Chat.Trace("# (^q^) 盤を 3 で埋めた☆\n")
+
+	// 盤上
+	for y := 0; y < boardSize; y++ {
+		for x := 0; x < boardSize; x++ {
+			board.SetColor(board.GetTIdxFromXy(x, y), 0)
+		}
+	}
+
+	// G.Chat.Trace("# (^q^) 石は置いた☆\n")
+
+	Moves = 0
+	KoIdx = 0
+
+	// G.Chat.Trace("# (^q^) 盤の初期化は終わったぜ☆\n")
+}
